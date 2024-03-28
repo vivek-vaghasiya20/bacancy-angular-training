ed4b06a2b6d1e67f375229e5f375f08ead67b782
vivek-vaghasiya20 <vivek.vaghasiya@bacancy.com>
2024-03-28 12:22:03 +0530

    upade task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'bacancy-angular-training';
+
+  user1: string = 'Vivek';
+
+  user2: any = {
+    id: 1,
+    firstName: 'Terry',
+    lastName: 'Medhurst',
+    maidenName: 'Smitham',
+    age: 50,
+    gender: 'male',
+  };
+
+  buttonInfo: string = 'hideInfo';
+  checkInfo: boolean = false;
+  updateInfo() {
+    this.checkInfo = !this.checkInfo;
+    this.buttonInfo = this.checkInfo ? 'showInfo' : 'hideInfo';
+  }
 }
