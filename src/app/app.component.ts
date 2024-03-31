d3dd91eae77bf3f7200137d3ca2fd2b4efac377f
vivek-vaghasiya20 <vivek.vaghasiya@bacancy.com>
2024-03-31 21:18:31 +0530

    first task commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'bacancy-angular-training';
+  user = {};
+  employees: any;
+
+  addEmp(): void {
+    this.employees.push('employee');
+  }
+
+  onRemoveEmp(id: number) {
+    const position = id + 1;
+    this.employees.splice(position, 1);
+  }
 }
