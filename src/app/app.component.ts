f70f02e0f3c94433366cd3e4942f1e491170176b
vivek-vaghasiya20 <vivek.vaghasiya@bacancy.com>
2024-04-19 12:04:52 +0530

    update form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'bacancy-angular-training';
+
+  @ViewChild('formObject') formData!: NgForm;
+
+  public onFormSubmit(): void {
+    console.log(this.formData);
+  }
 }
