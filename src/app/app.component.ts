ce9f09b71a3fa864764b7cf5decfdc3b61133532
vivek-vaghasiya20 <vivek.vaghasiya@bacancy.com>
2024-04-21 14:21:05 +0530

    update task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,37 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   title = 'bacancy-angular-training';
 
+  public showSubmittedData: boolean = false;
+  public defaultGender: string = 'male';
   @ViewChild('formObject') formData!: NgForm;
 
+  public fillForm(): void {
+    this.formData.form.setValue({
+      firstName: 'Vivek',
+      lastName: 'Vaghasiya',
+      email: 'vivek@example.com',
+      phoneNumber: 7485969685,
+      password: 'Vivek21',
+      gender: 'male',
+      City: 'surat',
+      remember_me: true,
+    });
+  }
+
+  public patchForm(): void {
+    this.formData.form.patchValue({
+      firstName: 'Vinay',
+      lastName: 'Vaghasiya',
+      email: 'vinay@example.com',
+    });
+  }
+
   public onFormSubmit(): void {
-    console.log(this.formData);
+    this.showSubmittedData = true;
+  }
+
+  public onFormReset(): void {
+    this.formData.reset();
+    this.showSubmittedData = false;
   }
 }
