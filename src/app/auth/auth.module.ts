5050cefc65d1e083b161b8e53441044e9f82238d
vivek-vaghasiya20 <vivek.vaghasiya@bacancy.com>
2024-05-06 23:00:25 +0530

    implement login component with template driven form.

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AuthRoutingModule } from './auth-routing.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [CommonModule, AuthRoutingModule, FormsModule],
   exports: [LoginComponent, RegisterComponent],
 })
 export class AuthModule {}
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-
+  @ViewChild('formObject') formData!: NgForm;
+  public onSubmit(): void {
+    console.log(
+      '%csrcappauthloginlogin.component.ts:12 this.formData',
+      'color: #007acc;',
+      this.formData
+    );
+  }
 }
