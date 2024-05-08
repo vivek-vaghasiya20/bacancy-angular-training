b22bac89954be444ab449607549a9fd3d5c4a9a4
vivek-vaghasiya20 <vivek.vaghasiya@bacancy.com>
2024-05-08 09:52:18 +0530

    implement header-footer for admin and user component.

--- src/app/admin/admin-header/admin-header.component.ts
+++ src/app/admin/admin-header/admin-header.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-admin-header',
   templateUrl: './admin-header.component.html',
-  styleUrls: ['./admin-header.component.scss']
+  styleUrls: ['./admin-header.component.scss'],
 })
 export class AdminHeaderComponent {
-
+  public logout() {}
 }
--- src/app/user/user-header/user-header.component.ts
+++ src/app/user/user-header/user-header.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user-header',
   templateUrl: './user-header.component.html',
-  styleUrls: ['./user-header.component.scss']
+  styleUrls: ['./user-header.component.scss'],
 })
 export class UserHeaderComponent {
-
+  public logout() {}
 }
