e74cc0b15c5498e9c307562f3dcf87e4efeed490
vivek-vaghasiya20 <vivek.vaghasiya@bacancy.com>
2024-05-27 17:13:04 +0530

    update code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,7 @@ export class AppComponent implements OnInit {
 
   constructor(public fb: FormBuilder) {
     //show alert on value change
-    effect(() => this.teamName());
+    effect(() => alert(this.teamName()));
   }
 
   ngOnInit(): void {
