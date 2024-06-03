c016c9785bd91109289a2f050287ada44310d5d3
vivek-vaghasiya20 <vivek.vaghasiya@bacancy.com>
2024-06-03 10:41:53 +0530

    change reference of of object and array of object from the parent.

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -19,12 +19,14 @@ export class ParentComponent {
   public increasePlayer(): void {
     if (this.newTeam) {
       this.newTeam.teamPlayer++;
+      this.newTeam = { ...this.newTeam };
     }
   }
 
   public decreasePlayer(): void {
     if (this.newTeam && this.newTeam.teamPlayer > 0) {
       this.newTeam.teamPlayer--;
+      this.newTeam = { ...this.newTeam };
     }
   }
 
@@ -35,15 +37,18 @@ export class ParentComponent {
 
   public increasePlayerOfArray(index: number): void {
     this.teamArray[index].teamPlayer++;
+    this.teamArray = [...this.teamArray];
   }
 
   public decreasePlayerOfArray(index: number): void {
     if (this.teamArray[index].teamPlayer > 0) {
       this.teamArray[index].teamPlayer--;
+      this.teamArray = [...this.teamArray];
     }
   }
 
   public deleteObjectOfArray(index: number): void {
     this.teamArray.splice(index, 1);
+    this.teamArray = [...this.teamArray];
   }
 }
