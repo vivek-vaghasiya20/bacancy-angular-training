e4b52636f3f434c55cb25555b706340f11a07b26
vivek-vaghasiya20 <vivek.vaghasiya@bacancy.com>
2024-06-03 22:10:42 +0530

    fire command dev:ssr,build:ssr and serve:ssr.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -9,7 +9,9 @@ import { SharedModule } from './shared/shared.module';
 @NgModule({
   declarations: [AppComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule, SharedModule],
-  providers: [],
+  providers: [
+    provideClientHydration()
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
